96239dffd25d900892493173c2ae2f5e36348323
paulaandrezza <paulaandrezza25@gmail.com>
2023-07-02 18:18:38 -0300

    finalizing modal

--- src/components/VideoGallery/VideoModal/index.tsx
+++ src/components/VideoGallery/VideoModal/index.tsx
@@ -1,12 +1,32 @@
+import { DividerX, Text } from '@/app/theme/shared';
 import { Video } from '@/types/videos';
-import { IoClose } from 'react-icons/io5';
-import { CloseButton, DivButton, ModalContent, ModalOverlay } from './styles';
+import { IoClose, IoCloudDownloadOutline } from 'react-icons/io5';
+import {
+  BlueText,
+  BoxTitle,
+  CloseButton,
+  Content,
+  ContentChild,
+  DivButton,
+  DownloadButton,
+  MiniModalContent,
+  ModalContent,
+  ModalOverlay,
+  RowButtons,
+} from './styles';
 
 interface Props {
   isOpen: boolean;
   onClose: () => void;
   video: Video | null;
 }
+
+const downloadsLabel = [
+  { content: 'Spreadsheet.xls', color: '#00AE7F', bg1: '#9FEFDF', bg2: '#C2F4EA' },
+  { content: 'Document.doc', color: '#0074EA', bg1: '#A1D9FF', bg2: '#C2E6FF' },
+  { content: 'Presentation.ppt', color: '#A99445', bg1: '#FFF1A0', bg2: '#FFF8D0' },
+  { content: 'Folder.zip', color: '#808080', bg1: '#D0DEE7', bg2: '#E8EEF3' },
+];
 
 const VideoModal = ({ isOpen, onClose, video }: Props) => {
   if (!isOpen) return null;
@@ -21,8 +41,55 @@ const VideoModal = ({ isOpen, onClose, video }: Props) => {
             <IoClose />
           </CloseButton>
         </DivButton>
-        {video?.title}
+        <BoxTitle>
+          <Text fontWeight="subtitle">
+            <BlueText>Webinar:</BlueText>
+            {video?.title}
+          </Text>
+        </BoxTitle>
+        <iframe
+          width="100%"
+          height="282"
+          src={video?.videoLink}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <Content>
+          <ContentChild>
+            <Text fontSize="small" fontWeight="bold">
+              Descrição
+            </Text>
+            <DividerX />
+            <Text fontSize="small">{video?.description}</Text>
+          </ContentChild>
+          <ContentChild>
+            <Text fontSize="small" fontWeight="bold">
+              Downloads
+            </Text>
+            <DividerX />
+            <RowButtons>
+              {downloadsLabel.slice(0, -1).map((item, index) => (
+                <DownloadButton key={index} color={item.color} bg1={item.bg1} bg2={item.bg2}>
+                  <IoCloudDownloadOutline />
+                  <div>{item.content}</div>
+                </DownloadButton>
+              ))}
+            </RowButtons>
+          </ContentChild>
+        </Content>
       </ModalContent>
+      <MiniModalContent
+        onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => e.stopPropagation()}
+      >
+        {downloadsLabel.map((item, index) => (
+          <DownloadButton key={index} color={item.color} bg1={item.bg1} bg2={item.bg2}>
+            <IoCloudDownloadOutline />
+            <div>{item.content}</div>
+          </DownloadButton>
+        ))}
+      </MiniModalContent>
     </ModalOverlay>
   );
 };
--- src/components/VideoGallery/VideoModal/styles.ts
+++ src/components/VideoGallery/VideoModal/styles.ts
@@ -14,14 +14,28 @@ export const ModalOverlay = styled.div`
 `;
 
 export const ModalContent = styled.div`
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 16px;
+  background-color: ${theme.colors['bg-primary']};
+  max-width: 500px;
+  border-radius: 8px;
+  border-top: 4px solid ${theme.colors['blue-secondary']};
+`;
+
+export const MiniModalContent = styled.div`
+  background-color: ${theme.colors['bg-primary']};
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
 `;
 
 export const CloseButton = styled.button`
   background-color: transparent;
   border: none;
+  padding: 8px;
   font-size: 18px;
   cursor: pointer;
   color: ${theme.colors['text-secondary']};
@@ -35,4 +49,58 @@ export const DivButton = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+`;
+
+export const BoxTitle = styled.div`
+  width: 60%;
+  text-align: center;
+  margin: 0 auto;
+  margin-bottom: 20px;
+`;
+
+export const BlueText = styled.span`
+  color: ${theme.colors['blue-secondary']};
+`;
+
+export const Content = styled.div`
+  margin: 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const ContentChild = styled.div``;
+
+export const RowButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`;
+
+interface DownloadButtonProps {
+  color: string;
+  bg1: string;
+  bg2: string;
+}
+
+export const DownloadButton = styled.button<DownloadButtonProps>`
+  color: ${(props) => (props.color ? props.color : '#00AE7F')};
+  background: ${(props) => (props.bg2 ? props.bg2 : '#9FEFDF')};
+  border: none;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+
+  cursor: pointer;
+
+  & > * {
+    padding: 8px;
+  }
+
+  & > :first-child {
+    background: ${(props) => (props.bg1 ? `${props.bg1}` : '#C2F4EA')};
+    border-radius: 8px 0 0 8px;
+    height: 100%;
+    width: 100%;
+  }
 `;
