e2593ca567a5ef393bd5ccf6fa301dff9c5787aa
paulaandrezza <paulaandrezza25@gmail.com>
2023-07-03 18:12:04 -0300

    atomizing footerLinks

--- src/components/Footer/Links/index.tsx
+++ src/components/Footer/Links/index.tsx
@@ -1,54 +1,17 @@
 'use client';
 
-import { Column, Flex, List, ListItem, Row, Text } from '@/app/theme/shared';
-import { FaFacebookF, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
-import { Wrapper } from '../styles';
+import { Flex } from '@/app/theme/shared';
+import LinkContact from './LinkContact';
+import LinkItem from './LinkItem';
 import { footerLinks } from './footerLinks';
-import { Label, Link, SocialBubble } from './styles';
 
 const Links = () => {
   return (
     <Flex $align="top">
       {footerLinks.map((item, index) => (
-        <Column key={index} $gap $align="left">
-          <Text fontWeight="bold">{item.category}</Text>
-          <List $align="start">
-            {item.links.map((link) => (
-              <ListItem key={link.name}>
-                <Link href={link.url} target="_blank">
-                  {link.name}
-                </Link>
-              </ListItem>
-            ))}
-          </List>
-        </Column>
+        <LinkItem key={index} item={item} />
       ))}
-      <Column $gap $align="left">
-        <Text fontWeight="bold">Siga a Leadster</Text>
-        <Wrapper $gap>
-          <Row>
-            <SocialBubble href="https://www.linkedin.com/company/getleadster/" target="_blank">
-              <FaLinkedinIn />
-            </SocialBubble>
-            <SocialBubble href="https://www.facebook.com/leadsterplatform/" target="_blank">
-              <FaFacebookF />
-            </SocialBubble>
-            <SocialBubble href="https://www.instagram.com/leadster.com.br/" target="_blank">
-              <FaInstagram />
-            </SocialBubble>
-          </Row>
-          <Wrapper>
-            <Text $textColor="secondary">
-              <Label>E-mail: </Label>
-              <Link href="mailto:contato@leadster.com.br">contato@leadster.com.br</Link>
-            </Text>
-            <Text $textColor="secondary">
-              <Label>Telefone: </Label>
-              <Link href="tel:+5542988289851">(42) 98828-9851</Link>
-            </Text>
-          </Wrapper>
-        </Wrapper>
-      </Column>
+      <LinkContact />
     </Flex>
   );
 };
--- src/components/Footer/Links/styles.ts
+++ src/components/Footer/Links/styles.ts
@@ -12,23 +12,3 @@ export const Link = styled.a`
     color: ${theme.colors['blue-primary']};
   }
 `;
-
-export const SocialBubble = styled.a`
-  background: ${theme.colors.bubble};
-  width: 48px;
-  height: 48px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  color: ${theme.colors['text-secondary']};
-
-  &:hover {
-    background: ${theme.colors['blue-primary']};
-    color: ${theme.colors['bg-primary']};
-  }
-`;
-
-export const Label = styled.span`
-  color: ${theme.colors['text-primary']};
-`;
