c1f3ee93bd5592b928de69479505dc4d3be65621
paulaandrezza <paulaandrezza25@gmail.com>
2023-07-03 14:46:59 -0300

    add animation with framer-motion

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,22 @@
 
 import { Container } from '@/app/theme/shared';
 import logo from '@public/logo.png';
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 
 const Header = () => {
   return (
     <Container>
-      <Image src={logo} alt="leadster-logo"></Image>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ rotate: 360, scale: 1 }}
+        transition={{
+          type: 'spring',
+          stiffness: 100,
+        }}
+      >
+        <Image src={logo} alt="leadster-logo"></Image>
+      </motion.div>
     </Container>
   );
 };
--- src/components/VideoGallery/VideoSection/VideoCard/index.tsx
+++ src/components/VideoGallery/VideoSection/VideoCard/index.tsx
@@ -3,7 +3,22 @@
 import { Text } from '@/app/theme/shared';
 import { Video } from '@/types/videos';
 import photo from '@public/thumbnail.png';
+import { motion, Variants } from 'framer-motion';
 import { Box, BoxImage, CardContent } from './styles';
+
+const cardVariants: Variants = {
+  offscreen: {
+    y: 50,
+  },
+  onscreen: {
+    y: 0,
+    transition: {
+      type: 'spring',
+      bounce: 0.4,
+      duration: 0.8,
+    },
+  },
+};
 
 interface Props extends Video {
   onClick: () => void;
@@ -11,12 +26,20 @@ interface Props extends Video {
 
 const VideoCard = ({ title, onClick }: Props) => {
   return (
-    <Box onClick={onClick}>
-      <BoxImage src={photo} alt="Prévia vídeo" />
-      <CardContent>
-        <Text fontWeight="bold">{title}</Text>
-      </CardContent>
-    </Box>
+    <motion.div initial="offscreen" whileInView="onscreen" viewport={{ once: true, amount: 0.8 }}>
+      <Box
+        as={motion.div}
+        whileHover={{ scale: 1.02 }}
+        transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+        variants={cardVariants}
+        onClick={onClick}
+      >
+        <BoxImage src={photo} alt="Prévia vídeo" />
+        <CardContent>
+          <Text fontWeight="bold">{title}</Text>
+        </CardContent>
+      </Box>
+    </motion.div>
   );
 };
 
